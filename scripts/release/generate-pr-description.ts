261d6e588ccdc959ece5bada7f4d741ff7baf257
Jeppe Reinhold <jeppe@reinhold.is>
2023-06-06 14:18:15 +0200

    generate pr descriptions with cherry picks

--- scripts/release/generate-pr-description.ts
+++ scripts/release/generate-pr-description.ts
@@ -20,12 +20,20 @@ program
   )
   .option('-N, --next-version <version>', 'Which version to generate changelog to, eg. "7.0.8"')
   .option('-P, --unpicked-patches', 'Set to only consider PRs labeled with "patch" label')
+  .option(
+    '-M, --manual-cherry-picks <commits>',
+    'A stringified JSON array of commit hashes, of patch PRs that needs to be cherry-picked manually'
+  )
   .option('-V, --verbose', 'Enable verbose logging', false);
 
 const optionsSchema = z.object({
   currentVersion: z.string().optional(),
   nextVersion: z.string().optional(),
   unpickedPatches: z.boolean().optional(),
+  manualCherryPicks: z
+    .string().default('[]')
+    .transform((val) => JSON.parse(val))
+    .refine((val) => Array.isArray(val)),
   verbose: z.boolean().optional(),
 });
 
@@ -33,12 +41,8 @@ type Options = {
   currentVersion?: string;
   nextVersion?: string;
   unpickedPatches?: boolean;
+  manualCherryPicks?: string[];
   verbose: boolean;
-};
-
-const validateOptions = (options: { [key: string]: any }): options is Options => {
-  optionsSchema.parse(options);
-  return true;
 };
 
 const LABELS_BY_IMPORTANCE = {
@@ -100,16 +104,48 @@ export const mapToChangelist = ({
     .join('\n');
 };
 
+const mapCherryPicksToTodo = ({
+  commits,
+  changes,
+  verbose,
+}: {
+  commits: string[];
+  changes: Change[];
+  verbose?: boolean;
+}): string => {
+  const list = commits
+    .map((commit) => {
+      const change = changes.find((change) => change.commit === commit.substring(0,7));
+      if(!change) {
+        throw new Error(`Cherry pick commit "${commit}" not found in changes, this should not happen?!`);
+      }
+      return `- [ ] ${change.links.pull}: \`git cherry-pick -m1 -x ${commit}\``;
+    })
+    .join('\n');
+
+  if (verbose) {
+    console.log(`🍒 Cherry pick list:\n${list}`);
+  }
+  return dedent`## 🍒 Manual cherry picking needed!
+
+  The following pull requests could not be cherry-picked automatically because it resulted in merge conflicts.
+  For each pull request below, you need to either manually cherry pick it, or discard it by removing the "patch" label from the PR and re-generate this PR.
+  
+  ${list}`;
+};
+
 export const generateReleaseDescription = ({
   currentVersion,
   nextVersion,
   changeList,
   changelogText,
+  manualCherryPicks
 }: {
   currentVersion: string;
   nextVersion: string;
   changeList: string;
   changelogText: string;
+  manualCherryPicks?: string;
 }): string => {
   return (
     dedent`This is an automated pull request that bumps the version from \`${currentVersion}\` to \`${nextVersion}\`.
@@ -134,6 +170,8 @@ export const generateReleaseDescription = ({
   This is a list of all the PRs merged and commits pushed directly to \`next\`, that will be part of this release:
   
   ${changeList}
+
+  ${manualCherryPicks ? manualCherryPicks : ''}
 
   If you've made any changes doing the above QA (change PR titles, revert PRs), manually trigger a re-generation of this PR with [this workflow](https://github.com/storybookjs/monorepo-release-tooling-prototype/actions/workflows/prepare-prerelease.yml) and wait for it to finish. It will wipe your progress in this to do, which is expected.
   
@@ -176,11 +214,8 @@ export const generateNonReleaseDescription = (changeList: string): string => {
   );
 };
 
-export const run = async (options: unknown) => {
-  if (!validateOptions(options)) {
-    return;
-  }
-  const { nextVersion, unpickedPatches, verbose } = options;
+export const run = async (rawOptions: unknown) => {
+  const { nextVersion, unpickedPatches, verbose, manualCherryPicks,...options } = optionsSchema.parse(rawOptions) as Options;
 
   if (!nextVersion) {
     console.log(
@@ -210,6 +245,13 @@ export const run = async (options: unknown) => {
         nextVersion,
         changeList: mapToChangelist({ changes, isRelease: true }),
         changelogText,
+        ...(manualCherryPicks?.length > 0 && {
+          manualCherryPicks: mapCherryPicksToTodo({
+            commits: manualCherryPicks,
+            changes,
+            verbose,
+          }),
+        }),
       })
     : generateNonReleaseDescription(mapToChangelist({ changes, isRelease: false }));
 
